050be6ab6d5c41f960122eae8d67bc909f50b05e
AMINE <amineelhailaa@gmail.com>
2026-04-14 19:39:49 +0100

    feat(websocket): add custom authorizer to echo config pointing to VITE_API_URL/broadcasting/auth using fetch with credentials

--- src/lib/echo.js
+++ src/lib/echo.js
@@ -1,16 +1,32 @@
-import Echo  from "laravel-echo";
-import Pusher from "pusher-js";
+import Echo from 'laravel-echo'
+import Pusher from 'pusher-js'
 
-window.Pusher = Pusher;
+window.Pusher = Pusher
 
 const echo = new Echo({
-    broadcaster: 'reverb',
-    key: import.meta.env.VITE_REVERB_APP_KEY,
-    wsHost: import.meta.env.VITE_REVERB_HOST,
-    wsPort: import.meta.env.VITE_REVERB_PORT,
-    wssPort: import.meta.env.VITE_REVERB_PORT,
-    forceTLS: false,
-    enabledTransports: ['ws', 'wss'],
+  broadcaster: 'reverb',
+  key: import.meta.env.VITE_REVERB_APP_KEY,
+  wsHost: import.meta.env.VITE_REVERB_HOST,
+  wsPort: import.meta.env.VITE_REVERB_PORT,
+  wssPort: import.meta.env.VITE_REVERB_PORT,
+  forceTLS: false,
+  enabledTransports: ['ws', 'wss'],
+  authorizer: (channel) => ({
+    authorize: (socketId, callback) => {
+      fetch(`${import.meta.env.VITE_API_URL}/broadcasting/auth`, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+          Accept: 'application/json',
+        },
+        body: JSON.stringify({ socket_id: socketId, channel_name: channel.name }),
+      })
+        .then((res) => res.json())
+        .then((data) => callback(null, data))
+        .catch((err) => callback(err))
+    },
+  }),
 })
 
 export default echo
